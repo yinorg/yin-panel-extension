d299a0ee92a499f6c46fd0bdc3e5d1dbec98673e
Alkaid <alkaid.ops@gmail.com>
2026-09-21 15:42:41 +0800

    fix: hide new tab redirect placeholder

--- newtab.js
+++ newtab.js
@@ -1,7 +1,4 @@
-const target = document.querySelector('#target')
-
 chrome.storage.sync.get({ [NEW_TAB_STORAGE_KEY]: DEFAULT_NEW_TAB_URL }).then(values => {
   const url = normalizeNewTabUrl(values[NEW_TAB_STORAGE_KEY]) || DEFAULT_NEW_TAB_URL
-  target.href = url
   location.replace(url)
 }).catch(() => location.replace(DEFAULT_NEW_TAB_URL))
